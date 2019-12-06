816d42c7a44d7b728cd9dd1a4a158ac9d86f817d
purwaren <and.thau@gmail.com>
2019-12-06 16:12:17 +0700

    add DateTimeExpired on CreateBillingResponse

--- dto/response.go
+++ dto/response.go
@@ -1,7 +1,10 @@
 package dto
 
+import "database/sql"
+
 type CreateBillingResponse struct {
     Status          string `json:"status"`
     VirtualAccount  string `json:"virtual_account"`
     TrxId           string `json:"trx_id"`
+    DateTimeExpired sql.NullTime `json:"datetime_expired"`
 }
